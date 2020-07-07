a86b9a7484c02e546edd48bfbbd0cf2d6a53a7ec
Sviatoslav Lementar <lemsviat@gmail.com>
2020-07-07 07:08:32 +0300

    First commit

---
+++ src/main/java/com/lemsviat/lemhomework134/CrudApplicationTxt.java
@@ -0,0 +1,4 @@
+package main.java.com.lemsviat.lemhomework134;
+
+public class CrudApplicationTxt {
+}
---
+++ src/main/java/com/lemsviat/lemhomework134/Skill.java
@@ -0,0 +1,17 @@
+package main.java.com.lemsviat.lemhomework134;
+
+public class Skill {
+    private String skillName;
+
+    public String getSkillName() {
+        return skillName;
+    }
+
+    public void setSkillName(String skillName) {
+        this.skillName = skillName;
+    }
+
+    public Skill(String skillName) {
+        this.skillName = skillName;
+    }
+}
